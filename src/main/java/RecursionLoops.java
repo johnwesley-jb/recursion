e3000708a93273e51cc7f518c84cb479bc629a34
johnwesley-jb <johnwesley.jnbaptiste@gmail.com>
2023-11-23 20:15:47 -0600

    facto, fibo commit

--- src/main/java/RecursionLoops.java
+++ src/main/java/RecursionLoops.java
@@ -23,11 +23,32 @@ public class RecursionLoops {
         whileLoop(n + 1, max);
     }
 
-    public static void main(String[] args) {
-        forLoop(0,100, 3);
-        whileLoop(0, 10);
-        doWhile(0, 10);
+    static int power(int number, int power) {
+        if (power <= 0) return 1;
+        return number * power(number, power - 1);
 
     }
+
+    static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    static int fibonacci(int n){
+        if (n<2) return n;
+        else return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+//        forLoop(0,100, 3);
+//        whileLoop(0, 10);
+//        doWhile(0, 10);
+        System.out.println(fibonacci(10));
+
+        System.out.println(power(10, 3));
+        System.out.println(factorial(6));
+
+    }
+
 
 }
