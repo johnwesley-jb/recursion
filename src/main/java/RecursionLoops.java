158d604e03958c66e1a12bd881d1f48bb12e3432
johnwesley-jb <johnwesley.jnbaptiste@gmail.com>
2023-11-23 19:05:33 -0600

    doWhile commit

--- src/main/java/RecursionLoops.java
+++ src/main/java/RecursionLoops.java
@@ -1,28 +1,33 @@
 
 public class RecursionLoops {
-    static void forLoop(int start, int limit, int increment ){
+    static void forLoop(int start, int limit, int increment) {
         System.out.println(start);
-        if(start>=limit-increment) return;
-        forLoop(start+increment, limit, increment);
+        if (start >= limit - increment) return;
+        forLoop(start + increment, limit, increment);
     }
 
-    static void whileLoop(int n, int max){
-        if (max==n) {
+    static void whileLoop(int n, int max) {
+        if (max == n) {
             return;
         }
         System.out.println(n);
-        whileLoop(n+1, max);
+        whileLoop(n + 1, max);
 
     }
 
-    static void doWhile(){
-
-
+    static void doWhile(int n, int max) {
+        System.out.println(n);
+        if (max == n) {
+            return;
+        }
+        whileLoop(n + 1, max);
     }
 
     public static void main(String[] args) {
-       forLoop(0,100, 3);
-       whileLoop(0, 10);
+        forLoop(0,100, 3);
+        whileLoop(0, 10);
+        doWhile(0, 10);
+
     }
 
 }
